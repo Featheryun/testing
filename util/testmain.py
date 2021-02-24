d9e390f164816bfc87db90aa46d17103dde5cd94
featherhello <1172445317@qq.com>
2021-02-24 17:35:15 +0800

    commit
    Signed-off-by: featherhello <1172445317@qq.com>

--- util/testmain.py
+++ util/testmain.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     report = BeautifulReport(discover)
     a = '用户信息(' + datedemo + ').html'
     report.report(filename=a, description='测试报告', report_dir='D:\\testing\\util', theme='theme_default')
-    send_email('D:\\testing\\util\\'+a)
+    # send_email('D:\\testing\\util\\'+a)
     # with open(pathro2, "wb")as file:
     #     runner = HTMLTestRunner(stream=file, title="测试报告", description="杨芸")
     #     runner.run(discover)
